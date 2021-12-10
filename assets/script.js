0984a3a5b5768a5c528836feb2829565b69ce96c
Austin Miller <austin.miller1424@gmail.com>
2021-12-10 12:56:35 -0600

    timer fix,question 1, question 2

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,52 @@
 var buttonEl = document.querySelector("#start-quiz");
+var h2El = document.querySelector("heading");
+var timer; 
 
-//when start button clicked start the time and the quiz
-//buttonEl.addEventListener("click", startTimer, testQuestion1);
-
-function testQuestion1 (){
-//define varaibles Questions 1-5 , answers 1-4 * 5 questions,timer, highscore    
+var testQuestion1 = function (){
+//create the element 
 var h2El = document.createElement("h2");
 var questionEl = document.createElement("p");
 var answersListEl = document.createElement("ul");
-var correctAnswerEL = document.createElement("li");
-var wrongAnswer1El = document.createElement ("li")
-var wrongAnswer2El = document.createElement ("li")
-var wrongAnswer3El = document.createElement ("li")
+var correctAnswerEL = document.createElement("button");
+var wrongAnswer1El = document.createElement ("button");
+var wrongAnswer2El = document.createElement ("button");
+var wrongAnswer3El = document.createElement ("button");
 
 
 //add text for heading, question, answers
 h2El.textContent = "QUESTION 1";
 questionEl.textContent = "How do you acess the console log in your browser?";
-answersListEl.textContent = (correctAnswerEL.textConten, wrongAnswer1El.textContent, wrongAnswer2El.textContentt, wrongAnswer3El.textContent);
-correctAnswerEL.textContent = "Right click the Browser";
+answersListEl.textContent = (correctAnswerEL.textContent, wrongAnswer1El.textContent, wrongAnswer2El.textContent, wrongAnswer3El.textContent);
+correctAnswerEL.textContent = "Right click the browser then click inspect";
 wrongAnswer1El.textContent = "Yell at the screen";
 wrongAnswer2El.textContent = "Pray";
 wrongAnswer3El.textContent = "All of the Above";
 
+correctAnswerEL.addEventListener("click", testQuestion2);
+wrongAnswer1El.addEventListener("click", subtractTime);
+wrongAnswer2El.addEventListener("click", subtractTime);
+wrongAnswer3El.addEventListener("click", subtractTime);
+
+var container = document.querySelector (".questions")
+    container.appendChild (h2El);
+    container.appendChild (questionEl);
+    container.appendChild (answersListEl);
+    answersListEl.appendChild (correctAnswerEL);
+    answersListEl.appendChild (wrongAnswer3El);
+    answersListEl.appendChild (wrongAnswer2El);
+    answersListEl.appendChild (wrongAnswer1El);
+
+var WelcomeMessage = document.querySelector (".be-gone")
+    WelcomeMessage.style.display = "none";
 };
 
 //countdown function 
-function startTimer(duration, display) {
-    var timer = duration,seconds;
+function startTimer(duration) {
+    timer = duration
+    var seconds;
     setInterval(function () {
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ? "6" + seconds : seconds;
-
+        seconds = timer
+        var display = document.querySelector('#time');
         display.textContent = seconds;
 
         if (--timer < 0) {
@@ -42,11 +56,57 @@ function startTimer(duration, display) {
 }
 
 buttonEl.onclick = function () {
-    var sixtySeconds = 60, 
-        display = document.querySelector('#time');
-    startTimer(sixtySeconds, display);
+    var sixtySeconds = 59
+        
+    startTimer(sixtySeconds);
+
+testQuestion1();
 };
 
+function nextQuestion () {
+    alert("next question")
+}
+
+function subtractTime (){
+    timer -= 10
+    testQuestion2 ();
+}
+
+var testQuestion2 = function (){
+    //create the element 
+    var h2El = document.createElement("h2");
+    var questionEl = document.createElement("p");
+    var answersListEl = document.createElement("ul");
+    var correctAnswerEL = document.createElement("button");
+    var wrongAnswer1El = document.createElement ("button");
+    var wrongAnswer2El = document.createElement ("button");
+    var wrongAnswer3El = document.createElement ("button");
+    
+    
+    //add text for heading, question, answers
+    h2El.textContent = "QUESTION 2";
+    questionEl.textContent = "Whats up?";
+    answersListEl.textContent = (correctAnswerEL.textContent, wrongAnswer1El.textContent, wrongAnswer2El.textContent, wrongAnswer3El.textContent);
+    correctAnswerEL.textContent = "nothing much you?";
+    wrongAnswer1El.textContent = "no";
+    wrongAnswer2El.textContent = "yes";
+    wrongAnswer3El.textContent = "bye";
+    
+    correctAnswerEL.addEventListener("click", nextQuestion);
+    wrongAnswer1El.addEventListener("click", subtractTime);
+    wrongAnswer2El.addEventListener("click", subtractTime);
+    wrongAnswer3El.addEventListener("click", subtractTime);
+    
+    var container = document.querySelector (".questions")
+        container.innerHTML = "";
+        container.appendChild (h2El);
+        container.appendChild (questionEl);
+        container.appendChild (answersListEl);
+        answersListEl.appendChild (correctAnswerEL);
+        answersListEl.appendChild (wrongAnswer3El);
+        answersListEl.appendChild (wrongAnswer2El);
+        answersListEl.appendChild (wrongAnswer1El);
+    };
 //if liCorrect is clicked then go to questions 2 
 //if else take 10 seconds off and still go to next question 
 
